6367b463456be77911b481168b08f49b55af2e2d
Jakob van Santen <jvansanten@gmail.com>
2010-01-29 18:07:21 +0000

    Update example converter to use __setitem__
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/hdf-writer/branches/new-internals-2@61349 16731396-06f5-0310-8873-f7f720988828

--- resources/scripts/test_pybindings.py
+++ resources/scripts/test_pybindings.py
@@ -143,20 +143,20 @@ class DOMLaunchBookie(hdf_writer.I3Converter):
 	def GetNumberOfRows(self,frameobj):
 		return 1
 	def Convert(self,domlaunch,rows,frame):
-		rows.set('start_time',domlaunch.GetStartTime())
-		rows.set('raw_charge_stamp',domlaunch.GetRawChargeStamp())
-		rows.set('pedestal_sub',domlaunch.GetIsPedestalSub())
-		rows.set('lc_bit',domlaunch.GetLCBit())
-		rows.set('trigger_type',domlaunch.GetTriggerType())
-		rows.set('trigger_mode',domlaunch.GetTriggerMode())
+		rows['start_time']       = domlaunch.GetStartTime()
+		rows['raw_charge_stamp'] = domlaunch.GetRawChargeStamp()
+		rows['pedestal_sub']     = domlaunch.GetIsPedestalSub()
+		rows['lc_bit']           = domlaunch.GetLCBit()
+		rows['trigger_type']     = domlaunch.GetTriggerType()
+		rows['trigger_mode']     = domlaunch.GetTriggerMode()
 		# we can pass I3VectorInts
-		rows.set('raw_atwd_0',domlaunch.GetRawATWD(0))
-		# ooooor arrays with the proper typcode
-		rows.set('raw_atwd_1',array.array('i',domlaunch.GetRawATWD(1)))
+		rows['raw_atwd_0']       = domlaunch.GetRawATWD(0)
+		# ooooor arrays with the proper typecode
+		rows['raw_atwd_1']       = array.array('i',domlaunch.GetRawATWD(1))
 		# or even numpy ndarrays!
-		rows.set('raw_atwd_2',numpy.array(domlaunch.GetRawATWD(2),'i'))
+		rows['raw_atwd_2']       = numpy.array(domlaunch.GetRawATWD(2),'i')
 	
-		rows.set('raw_fadc',array.array('H',domlaunch.GetRawFADC()))
+		rows['raw_fadc']         = array.array('H',domlaunch.GetRawFADC())
 		return 1
 		
 def test(fname='/Users/jakob/Documents/IceCube/nugen_nue_ic80_dc6.001568.000000.hits.001.1881140.domsim.001.2028732.i3.gz'):
