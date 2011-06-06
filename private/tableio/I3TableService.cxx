b3ff4740a256da951e57b5a9271ad91f7c319ce5
Jakob van Santen <jvansanten@gmail.com>
2011-06-06 20:09:51 +0000

    Shared the ticConverter_ between TableWriter and TableService to avoid nasty surprises in the index table.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/tableio/trunk@76184 16731396-06f5-0310-8873-f7f720988828

--- private/tableio/I3TableService.cxx
+++ private/tableio/I3TableService.cxx
@@ -16,13 +16,8 @@
 /******************************************************************************/
 
 I3TableService::I3TableService()  { 
-    ticConverter_ = BuildConverter("I3IndexColumnsGenerator");
-    // My house, my rules.
-    I3TableRowDescriptionPtr desc =
-        boost::const_pointer_cast<I3TableRowDescription>(ticConverter_->GetDescription(I3EventHeader()));
-    desc->AddField<tableio_size_t>("start", "", "Offset at which the rows for this event start");
-    desc->AddField<tableio_size_t>("stop", "", "Offset at which the rows for the next event start");
-    indexDescription_ = desc;
+    // Set up a semi-sensible default
+    SetIndexConverter(BuildConverter("I3IndexColumnsGenerator"));
 }
 
 /******************************************************************************/
@@ -110,6 +105,17 @@ I3TableRowDescriptionConstPtr I3TableService::GetIndexDescription() {
    return indexDescription_;
 }
 
+/******************************************************************************/
+
+void I3TableService::SetIndexConverter(I3ConverterPtr gen) {
+    ticConverter_ = gen;
+    // My house, my rules.
+    I3TableRowDescriptionPtr desc =
+        boost::const_pointer_cast<I3TableRowDescription>(ticConverter_->GetDescription(I3EventHeader()));
+    desc->AddField<tableio_size_t>("start", "", "Offset at which the rows for this event start");
+    desc->AddField<tableio_size_t>("stop", "", "Offset at which the rows for the next event start");
+    indexDescription_ = desc;
+}
 
 /******************************************************************************/
         
