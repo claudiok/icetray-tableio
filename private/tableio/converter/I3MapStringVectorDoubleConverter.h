5227d594cfca6d3a8ccdfcaf383a49286c663035
Sebastian Schoenen <schoenen@physik.rwth-aachen.de>
2011-11-15 15:25:11 +0000

    add a converter for I3MapStringVectorDouble
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/tableio/trunk@81690 16731396-06f5-0310-8873-f7f720988828

--- private/tableio/converter/I3MapStringVectorDoubleConverter.h
+++ private/tableio/converter/I3MapStringVectorDoubleConverter.h
@@ -1,14 +1,3 @@
-/**
- * copyright  (C) 2010
- * The Icecube Collaboration
- *
- * $Id: I3MapStringVectorDoubleConverter.h 64296 2010-05-25 16:13:48Z jvansanten $
- *
- * @version $Revision: 64296 $
- * @date $LastChangedDate: 2010-05-25 18:13:48 +0200 (Tue, 25 May 2010) $
- * @author Jakob van Santen <vansanten@wisc.edu> $LastChangedBy: jvansanten $
- */
-
 #ifndef TABLEIO_I3MAPSTRINGVECTORDOUBLECONVERTER_H_INCLUDED
 #define TABLEIO_I3MAPSTRINGVECTORDOUBLECONVERTER_H_INCLUDED
 
