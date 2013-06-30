beb4b8323cfbda9c4eae29f3265a86019fb9fbab
Nathan Whitehorn <nwhitehorn@icecube.wisc.edu>
2013-06-30 15:27:36 +0000

    Deduplicate tableio enum definitions. Keep imports in both places so that the overall namespace is unchanged.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/tableio/trunk@107691 16731396-06f5-0310-8873-f7f720988828

--- python/types.py
+++ python/types.py
@@ -41,49 +41,6 @@ def pretty_dtype(self):
     
 I3Datatype.__repr__ = pretty_dtype
 
-class baseEnum(int):
-	name = None
-	values = {}
-	def __repr__(self):
-		return self.name
+del I3Datatype,dt,make_dtype,pretty_dtype
 
-class metaEnum(type):
-	"Helper metaclass to return the class variables as a dictionary "
-
-	def __new__(cls, classname, bases, classdict):
-		""" Return a new class with a "values" attribute filled
-		"""
-
-		newdict = {"values":{}}
-
-		for k in classdict.keys():
-			if not (k.startswith('_') or k == 'name' or k == 'values'):
-				val = classdict[k]
-				member = baseEnum(val)
-				member.name = k
-				newdict['values'][val] = member
-				newdict[k] = member
-		
-		# Tell each member about the values in the enum
-		for k in newdict['values'].keys():
-			newdict['values'][k].values = newdict['values']
-		# Return a new class with the "values" attribute filled
-		return type.__new__(cls, classname, bases, newdict)
-
-class Enum(baseEnum):
-	"""This class mimicks the interface of boost-python-wrapped enums.
-	
-Inherit from this class to construct enumerated types that can
-be passed to the I3Datatype, e.g.:
-
-	class DummyEnummy(tableio.types.Enum):
-		Foo = 0
-		Bar = 1
-		Baz = 2
-
-	desc = tableio.I3TableRowDescription()
-	desc.add_field('dummy', tableio.I3Datatype(DummyEnummy), '', '')
-"""
-	__metaclass__ = metaEnum
-
-del I3Datatype,dt,make_dtype,pretty_dtype,baseEnum,metaEnum
+from enum import enum as Enum
