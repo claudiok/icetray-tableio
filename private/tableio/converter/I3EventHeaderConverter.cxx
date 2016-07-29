9d535fde93a7e15c4c0d42b870b1121c083e1368
Jakob van Santen <jvansanten@gmail.com>
2016-07-29 09:10:23 +0000

    Replace fractured but precise representation of event start and end in MJD with a double-precision value. This introduces a discretization of 300 ns that will increase to 600 ns in the next 100 years and 2500 in the next 1000. These are much smaller than the duration of a typical event, and can be ignored in coordinate calculations. Fixes #1550.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/tableio/trunk@148889 16731396-06f5-0310-8873-f7f720988828

--- private/tableio/converter/I3EventHeaderConverter.cxx
+++ private/tableio/converter/I3EventHeaderConverter.cxx
@@ -15,21 +15,9 @@ I3TableRowDescriptionPtr I3EventHeaderConverter::CreateDescription(const I3Event
     
     I3TableRowDescriptionPtr desc(new I3TableRowDescription());
     desc->AddField<int64_t>("time_start_utc_daq", "1e-10 s", "start of event in daq time - tenths of nanoseconds since the start of the UTC year");
-    // utc times seem to be redundant 
-    //desc->AddField<int32_t>("time_start_utc_year","",        "start of event utc time - year");
-    //desc->AddField<int32_t>("time_start_utc_sec", "s",       "start of event in utc time - seconds");
-    //desc->AddField<double> ("time_start_utc_ns",  "ns",      "start of event in utc time - nanoseconds");
-    desc->AddField<int32_t>("time_start_mjd_day", "days",    "start of event in Modified Julian Days - days");
-    desc->AddField<int32_t>("time_start_mjd_sec", "seconds", "start of event in Modified Julian Days - seconds");
-    desc->AddField<double> ("time_start_mjd_ns",  "ns",      "start of event in Modified Julian Days - nanoseconds");
-    
-    desc->AddField<int64_t>("time_end_utc_daq", "1e-10 s", "end of event in daq time - tenths of nanoseconds since the start of the UTC year");
-    //desc->AddField<int32_t>("time_end_utc_year","",        "end of event in utc time - year");
-    //desc->AddField<int32_t>("time_end_utc_sec", "s",       "end of event in utc time - seconds");
-    //desc->AddField<double> ("time_end_utc_ns",  "ns",      "end of event in utc time - nanoseconds");
-    desc->AddField<int32_t>("time_end_mjd_day", "days",    "end of event in Modified Julian Days - days");
-    desc->AddField<int32_t>("time_end_mjd_sec", "seconds", "end of event in Modified Julian Days - seconds");
-    desc->AddField<double> ("time_end_mjd_ns",  "ns",      "end of event in Modified Julian Days - nanoseconds");
+    desc->AddField<double> ("time_start_mjd",     "days",    "start of event in Modified Julian Days");
+    desc->AddField<int64_t>("time_end_utc_daq",   "1e-10 s", "end of event in daq time - tenths of nanoseconds since the start of the UTC year");
+    desc->AddField<double> ("time_end_mjd",       "days",    "end of event in Modified Julian Days");
     return desc;
 }
     
@@ -39,14 +27,10 @@ size_t I3EventHeaderConverter::FillRows(const I3EventHeader& header,
     I3Time end = header.GetEndTime();
 
     rows->Set<int64_t>("time_start_utc_daq", start.GetUTCDaqTime());
-    rows->Set<int32_t>("time_start_mjd_day", start.GetModJulianDay());
-    rows->Set<int32_t>("time_start_mjd_sec", start.GetModJulianSec());
-    rows->Set<double >("time_start_mjd_ns",  start.GetModJulianNanoSec());
+    rows->Set<double >("time_start_mjd",  start.GetModJulianDayDouble());
     
     rows->Set<int64_t>("time_end_utc_daq", end.GetUTCDaqTime());
-    rows->Set<int32_t>("time_end_mjd_day", end.GetModJulianDay());
-    rows->Set<int32_t>("time_end_mjd_sec", end.GetModJulianSec());
-    rows->Set<double >("time_end_mjd_ns",  end.GetModJulianNanoSec());
+    rows->Set<double >("time_end_mjd",  end.GetModJulianDayDouble());
 
     return 1;
 }
